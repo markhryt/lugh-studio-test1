a71a40222f6356622fd8c26c4ca80ea0ee614c8e
markhryt <hrytchakmark@gmail.com>
2023-07-31 15:20:46 -0400

    email sending seted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import "./index.css"
+
 
 const button = document.getElementById("contactbutton")
 
-button.addEventListener('click', ()=>{
-    console.log('contacted');
+button.addEventListener('click',  function contactUs(event){
+    event.preventDefault();
+    const fullName = document.getElementById('fname').value;
+    const company = document.getElementById('company').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Log form field values
+    console.log('Full Name:', fullName);
+    console.log('Company:', company);
+    console.log('Email:', email);
+    console.log('Message:', message);
+    (function(){
+        emailjs.init("i9P0tanq7Q5bqkfPF");
+        emailjs.send("service_52dmlph", 'my_first_template', {from_name:fullName,
+            company:company, 
+            email:email,
+            message: message,
+            reply_to:"reply"},
+             "i9P0tanq7Q5bqkfPF");
+     })();
 })
+
+
+
